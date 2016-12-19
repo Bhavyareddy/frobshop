e5ad7c3b000829a08b4c9d984fbb027fde01c6d5
Bhavyareddy <bhavya@ideathrusts.com>
2016-12-19 14:36:01 +0530

    first commit

---
+++ application/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+# Create your models here.
+class Table(models.Model):
+	username = models.CharField(max_length= 30)
+	password = models.CharField(max_length = 30)
